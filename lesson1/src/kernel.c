ba0abfe457031481b607c09b9eddda6d270ebba8
Preetham Uchil <puchil@starbucks.com>
2020-08-11 11:23:50 -0700

    added mailbox logic

--- lesson1/src/kernel.c
+++ lesson1/src/kernel.c
@@ -1,9 +1,32 @@
 #include "mini_uart.h"
+#include "mailbox.h"
+
+void print_serial_num()
+{
+	// get the board's unique serial number with a mailbox call
+    mbox[0] = 8*4;                  // length of the message
+    mbox[1] = MBOX_REQUEST;         // this is a request message
+    mbox[2] = MBOX_TAG_GETSERIAL;   // get serial number command
+    mbox[3] = 8;                    // buffer size
+    mbox[4] = 8;
+    mbox[5] = 0;                    // clear output buffer
+    mbox[6] = 0;
+    mbox[7] = MBOX_TAG_LAST;
+
+    if(mbox_call(MBOX_CH_PROP)){
+	    uart_send_string("Serial Number: ");
+	    uart_send_hex(mbox[6]);
+	    uart_send_hex(mbox[5]);
+    } else {
+    	uart_send_string("Error: Unable to query Serial Number!");
+    }
+}
 
 void kernel_main( void )
 {
 	uart_init();
-	uart_send_string("Hello, World!\n");
+	uart_send_string("Hello, Minix 4!\n");
+	print_serial_num();
 	while(1)
 		uart_send(uart_recv());
 }
--- lesson1/src/mini_uart.c
+++ lesson1/src/mini_uart.c
@@ -17,6 +17,21 @@ void uart_send_string(char* str)
 		uart_send((char)str[i]);
 }
 
+/**
+ * Display a binary value in hexadecimal
+ */
+void uart_send_hex(unsigned int d) {
+    unsigned int n;
+    int c;
+    for(c=28;c>=0;c-=4) {
+        // get highest tetrad
+        n=(d>>c)&0xF;
+        // 0-9 => '0'-'9', 10-15 => 'A'-'F'
+        n+=n>9?0x37:0x30;
+        uart_send(n);
+    }
+}
+
 char uart_recv( void )
 {
 	while(1){
