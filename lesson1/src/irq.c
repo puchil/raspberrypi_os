80ef0bc3e55ff6f51afe262abf0ecceb6b671225
Preetham Uchil <puchil@starbucks.com>
2020-09-02 06:22:31 -0700

    Fixed IRQ addresses for Pi4

--- lesson1/src/irq.c
+++ lesson1/src/irq.c
@@ -27,7 +27,7 @@ const char *entry_error_messages[] = {
 
 void enable_interrupt_controller(void)
 {
-	put32(ENABLE_IRQS_1, SYSTEM_TIMER_IRQ_1);
+	put32(IRQ0_SET_EN_0, SYSTEM_TIMER_IRQ_1);
 }
 
 void show_invalid_entry_message(int type, unsigned long esr, unsigned long address)
@@ -37,7 +37,7 @@ void show_invalid_entry_message(int type, unsigned long esr, unsigned long addre
 
 void handle_irq(void)
 {
-	unsigned int irq = get32(IRQ_PENDING_1);
+	unsigned int irq = get32(IRQ0_PENDING0);
 	switch(irq) {
 		case SYSTEM_TIMER_IRQ_1:
 			handle_timer_irq();
